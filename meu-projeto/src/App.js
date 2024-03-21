6a9c450db4e736e670e7917514fa10160afc68f1
Fernanda <fernanda.moura.cruz98@gmail.com>
2024-03-21 13:48:04 -0300

    Trabalhando com eventos no React

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,13 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
+import Evento from './components/Evento';
+import Form from './components/Form';
+/*
 import HelloWorld from './components/HelloWorld';
 import Frase from './components/Frase';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import List from './components/List';
+*/
+
 
 function App() {
 
+  /*
   const name = 'Fernanda';
   const newName = name.toUpperCase();
   const url ='https://via.placeholder.com/150';
@@ -16,10 +22,21 @@ function App() {
   function sum(a,b){
     return a+b;
   }
-
+  */
   return (
-    //No html, podemos utilizar o class, mas se tratando do react o correto é utilizar "className"
+
+   
     <div className="App">
+      <h1>Testando Eventos</h1>
+      <Evento numero ={1}/>
+      <Evento numero ={2}/>
+      <Form/>
+    </div>
+    
+    //No html, podemos utilizar o class, mas se tratando do react o correto é utilizar "className"
+    /*
+    <div className="App">
+      
       <h1>Hello World!</h1>
       <p>Olá, {newName}</p>
       <p>Soma: {sum(1,2)}</p>
@@ -30,7 +47,10 @@ function App() {
       <SayMyName nome={nomeExemplo}/>
       <Pessoa nome="João" idade="20" profissao="desenvolvedor web" foto="https://via.placeholder.com/150"/>
       <List/>
+      
     </div>
+    */
+   
   );
 }
 
