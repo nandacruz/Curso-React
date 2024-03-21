5f5faedacdec08e8a504fe5bee73443530273399
Fernanda <fernanda.moura.cruz98@gmail.com>
2024-03-21 11:58:34 -0300

    Avançando em props
    Definição de propriedades e tratamentos default

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,10 +1,28 @@
-function Item(props){
+
+//Pacote
+import PropTypes from 'prop-types';
+
+function Item({marca,ano_lancamento}){
     return(
         <>
-            <li>{props.marca}</li>
+            <li>{marca} - {ano_lancamento}</li>
         </>
 
     );
+}
+
+//Propriedade do componente
+Item.protoTypes = {
+    //Aqui foi definido o tipo, alem de colocar como required
+    marca: PropTypes.string.isRequired,
+    ano_lancamento: PropTypes.number.isRequired,
+}
+
+
+//Propriedade do componente default
+Item.defaultProps = {
+    marca: 'Faltou a marca',
+    ano_lancamento: 0,
 }
 
 export default Item;
--- meu-projeto/src/components/List.js
+++ meu-projeto/src/components/List.js
@@ -5,12 +5,14 @@ function List(){
         <>  
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Fiat"/>
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="Fiat" ano_lancamento={1964}/>
+                <Item marca="Renault" ano_lancamento={1964}/>
             </ul>
 
         </>
     );
 }
+
 
 export default List;
